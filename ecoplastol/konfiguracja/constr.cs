e97ae62fee42778cb80ff33d920cbec7674f5a71
marcin <marcin@zaczynski.pl>
2019-04-16 21:40:31 +0200

    connection string początek

---
+++ ecoplastol/konfiguracja/constr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecoplastol.konfiguracja
+{
+    class constr
+    {
+        public string adres { get; set; }
+        public string db { get; set; }
+        public string user { get; set; }
+        public string pass { get; set; }
+
+        public constr()
+        {
+            adres = "localhost";
+            db = "ecoplastol";
+            user = "postgres";
+            pass = "postgres";
+        }
+    }
+}
